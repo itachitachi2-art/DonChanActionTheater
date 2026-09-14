833b1cc1fe39df91dcf1d12fe2897b92ae5aca21
Itachidon <itachitachi2@gmail.com>
2026-09-14 21:45:46 +0900

    Remove obsolete Harmony build parameter

--- Standalone/QuietQuestDistance/build.ps1
+++ Standalone/QuietQuestDistance/build.ps1
@@ -4,9 +4,8 @@ param(
 
 $ErrorActionPreference = "Stop"
 $managed = Join-Path $GameDir "7DaysToDie_Data\Managed"
-$harmony = Join-Path $GameDir "Mods\0_TFP_Harmony"
 $project = Join-Path $PSScriptRoot "QuietQuestDistance.csproj"
 
-dotnet build $project -c Release -p:GameManagedDir="$managed" -p:HarmonyDir="$harmony"
+dotnet build $project -c Release -p:GameManagedDir="$managed"
 Copy-Item (Join-Path $PSScriptRoot "bin\Release\net48\QuietQuestDistance.dll") $PSScriptRoot -Force
 Write-Host "QuietQuestDistance.dll を作成しました。"
